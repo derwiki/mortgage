4d8f3cb8e7ff56226ebb1aa1c6185bcbb150654d
Adam Derewecki <derewecki@gmail.com>
2017-02-05 20:33:18 -0800

    Add HOA

--- mortgage_overpay_calculator.rb
+++ mortgage_overpay_calculator.rb
@@ -18,38 +18,40 @@ puts monthly_mortgage_payment + monthly_property_tax - monthly_property_tax_adju
 puts "monthly mortgage payment: #{monthly_mortgage_payment.round}"
 
 months_to_payoff_by_monthly_payment = {}
-[monthly_mortgage_payment, 3500, 4000, 4500, 5000].each do |target_monthly_payment|
-  running_principal = principal
-  running_value = home_value
-  #hoa = 0
-  (30 * 12).times do |month|
-    amounts = {}
-    amounts[:interest] = monthly_interest_rate * running_principal
-    amounts[:principal] = monthly_mortgage_payment - amounts[:interest]
-    amounts[:principal] += (target_monthly_payment - monthly_mortgage_payment)
-    running_principal -= amounts[:principal]
-    amounts[:property_tax] = monthly_property_tax
-    #amounts[:hoa] = hoa
-    amounts[:deductible] = amounts[:interest] + monthly_property_tax
-    amounts[:deduction_savings] = amounts[:deductible] * 0.36
-    amounts[:adjusted_monthly_payment] = target_monthly_payment - amounts[:deduction_savings]
-    amounts[:running_principal] = running_principal
-    amounts[:running_value] = (running_value *= 1.00583) # 1.07 / 12 months
-    amounts[:ownership] = home_value - running_principal
-    amounts[:ownership_percent] = amounts[:ownership] / home_value
-    amounts[:total_paid] = month * target_monthly_payment + deposit
-    # profit_rate = amounts[:running_value] / home_value
-    # this still doesn't seem right..
-    amounts[:running_profit] = (running_value - amounts[:total_paid]) * amounts[:ownership_percent]
+[0, 300, 500, 800].each do |hoa|
+# [monthly_mortgage_payment, 3500, 4000, 4500, 5000].each do |target_monthly_payment|
+  [4000].each do |target_monthly_payment|
+    running_principal = principal
+    running_value = home_value
+    (30 * 12).times do |month|
+      amounts = {}
+      amounts[:interest] = monthly_interest_rate * running_principal
+      amounts[:principal] = monthly_mortgage_payment - amounts[:interest]
+      amounts[:principal] += (target_monthly_payment - monthly_mortgage_payment - hoa)
+      running_principal -= amounts[:principal]
+      amounts[:property_tax] = monthly_property_tax
+      amounts[:hoa] = hoa
+      amounts[:deductible] = amounts[:interest] + monthly_property_tax
+      amounts[:deduction_savings] = amounts[:deductible] * 0.36
+      amounts[:adjusted_monthly_payment] = target_monthly_payment - amounts[:deduction_savings]
+      amounts[:running_principal] = running_principal
+      amounts[:running_value] = (running_value *= 1.00583) # 1.07 / 12 months
+      amounts[:ownership] = home_value - running_principal
+      amounts[:ownership_percent] = amounts[:ownership] / home_value
+      amounts[:total_paid] = month * target_monthly_payment + deposit
+      # profit_rate = amounts[:running_value] / home_value
+      # this still doesn't seem right..
+      amounts[:running_profit] = (running_value - amounts[:total_paid]) * amounts[:ownership_percent]
 
-    #puts Hash[amounts.map {|k,v| [k,v.to_i]}]
-    if month == 7 * 12
-      puts " ** 7 years, $#{target_monthly_payment.round(2)}/month, #{amounts.delete(:running_value).round}"
-      puts Hash[amounts.map {|k,v| [k,v.round(2)]}]
-      puts
+      #puts Hash[amounts.map {|k,v| [k,v.to_i]}]
+      if month == 7 * 12
+        puts " ** 7 years, $#{target_monthly_payment.round(2)}/month, #{amounts.delete(:running_value).round}, HOA: #{hoa.round}"
+        puts Hash[amounts.map {|k,v| [k,v.round(2)]}]
+        puts
+      end
+      months_to_payoff_by_monthly_payment[target_monthly_payment.round] = month
+      break if running_principal < 0
     end
-    months_to_payoff_by_monthly_payment[target_monthly_payment.round] = month
-    break if running_principal < 0
   end
 end
 puts months_to_payoff_by_monthly_payment
